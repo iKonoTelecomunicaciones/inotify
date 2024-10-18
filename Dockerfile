a8353a3e638ef41b79c44671ffc5b664dc8967d2
fduque <fduque@fduque.fduque@ikono.com.co>
2024-10-18 15:58:57 -0500

    Add asterisk package and fixed monitoring mode

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,28 @@
 FROM alpine:3.19.1
 
+# Install required packages
 RUN apk add -U \
         bash \
         curl \
         inotify-tools \
         netcat-openbsd \
         net-tools \
-        wget
+        wget \
+        sox \
+        libogg \
+        libvorbis \
+        asterisk \
+        asterisk-sample-config
 
+# Copy scripts
 COPY scripts/ /
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 
+# Copy asterisk configuration
+#COPY conf/* /etc/asterisk/
+
+# Run the entrypoint script
 ENTRYPOINT [ "/docker-entrypoint.sh" ]
+
+# Run the inotify-script
 CMD [ "inotify-script" ]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,6 +14,22 @@ if [ -z "${INOTIFY_SCRIPT+x}" ]; then
     exit 1
 fi
 
+# Add execution permission to INOTIFY_SCRIPT
+chmod +x "${INOTIFY_SCRIPT}"
+
+# Load modules
+asterisk_modules_dir="/usr/lib/asterisk/modules"
+new_modules_dir="/opt/volumes/modules"
+for module in $(ls $new_modules_dir/); do
+    if [ ! -f $asterisk_modules_dir/$module ]; then
+        cp -v $new_modules_dir/$module $asterisk_modules_dir/
+        chmod u+x $asterisk_modules_dir/$module
+    fi
+done
+
+# Start asterisk
+asterisk -f &
+
 if [ "$1" = "inotify-script" ]; then
     echo "[$(date -Iseconds)] running ${INOTIFYWAIT_SCRIPT}"
     exec "${INOTIFYWAIT_SCRIPT}" "${INOTIFY_TARGET}" "${INOTIFY_SCRIPT}"
